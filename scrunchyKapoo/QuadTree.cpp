d32307f79333875567432408e30ef3f5105e4767
Simon Johanson <marsvin2002@hotmail.com>
2017-02-14 13:40:20 +0100

    Added class QuadTree

---
+++ scrunchyKapoo/QuadTree.cpp
@@ -0,0 +1,10 @@
+#include "QuadTree.h"
+
+QuadTree::QuadTree()
+{}
+
+QuadTree::QuadTree( const QuadTree& other )
+{}
+
+QuadTree::~QuadTree()
+{}
---
+++ scrunchyKapoo/QuadTree.h
@@ -0,0 +1,12 @@
+#ifndef _QUADTREE_H_
+#define _QUADTREE_H_
+
+class QuadTree
+{
+	public:
+		QuadTree();
+		QuadTree( const QuadTree& other );
+		~QuadTree();
+
+};
+#endif
